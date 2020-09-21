b3e1a7f478596fbdc1e1af2f4f3383f208ddb5c0
Lindy Lim <lindylimlw@gmail.com>
2020-09-21 09:57:22 +0800

    Updates

---
+++ precache-manifest.c0126e8aa54fc3273db4363336028fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a16d40fc12265eaebcf0ff646c825db",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "16a5e28e1402da7757a2",
+    "url": "/personal/static/css/main.935e02c5.chunk.css"
+  },
+  {
+    "revision": "2fa637f60b9da01f84ce",
+    "url": "/personal/static/js/2.adbcda86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal/static/js/2.adbcda86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a5e28e1402da7757a2",
+    "url": "/personal/static/js/main.1067bbba.chunk.js"
+  },
+  {
+    "revision": "4e9c853d29b1df4846b9",
+    "url": "/personal/static/js/runtime-main.b0a78c69.js"
+  }
+]);

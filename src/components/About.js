97a48209d248cb37f5bc72169cb636960f9bd6a3
lindylim2019 <89002191+lindylim2019@users.noreply.github.com>
2021-09-23 11:06:19 +0800

    Updated Portfolio and About

--- src/components/About.js
+++ src/components/About.js
@@ -6,28 +6,22 @@ const About = () => {
         <div className="container">
             <br />
             <br />
-            <p>A second-year Computer Science undergraduate at <a href="https://www.smu.edu.sg/" target="_blank">Singapore Management University (SMU)</a>, I have a varied range of skills and interests, and am actively seeking internships (for the summer and December breaks) to continue to learn and grow as well as contribute to an organisation.</p>
+            <p>A third-year Computer Science undergraduate at <a href="https://www.smu.edu.sg/" target="_blank">Singapore Management University (SMU)</a>, I have a varied range of skills and interests, and am actively seeking internships (for the summer and December breaks) to continue to learn and grow as well as contribute to an organisation.</p>
             <p>I've had experience / am familiar with:</p>
             <ul className="show-points">
                 <li>Web development (HTML, CSS, JavaScript - vanilla, jQuery, React)</li>
-                <li>Building Java applications</li>
                 <li>Game development with Unity</li>
+                <li>Amazon Web Services</li>
+                <li>Building Java applications</li>
                 <li>Databases with MySQL</li>
                 <li>Python for general programming and data analysis</li>
                 <li>C for general programming</li>
                 <li>Agile practices</li>
-                <li>Basic ethical hacking</li>
+                <li>DevOps tools</li>
             </ul>
             <br />
-            <p>Some additional stuff that usually do not make it into my professional profiles:</p>
+            <p>Additional Stuff:</p>
             <ul>
-                <li>
-                    <div>
-                        <h6><b>Computer Graphics</b></h6>
-                        <p>I have a growing interest in the field of computer graphics, both from the perspective of programming and art. Currently, I have been exploring the art side, experimenting with 3D software like Blender and Maya. I plan to explore the programming side in the near future.</p>
-                    </div>
-                </li>
-
                 <li>
                     <div>
                         <h6><b>Rails Girls</b></h6>
@@ -37,13 +31,6 @@ const About = () => {
                             <li><a href="https://blog.surialabs.com/empowering-women-in-tech-rails-girls-kl/">SuriaLabs: EMPOWERING WOMEN IN TECH</a></li>
                             <li><a href="https://www.makchic.com/rails-girls-kuala-lumpur-opening-the-world-of-coding-to-girls/">MakChic: Opening the world of coding to girls</a></li>
                         </ul>
-                    </div>
-                </li>
-
-                <li>
-                    <div>
-                        <h6><b>Events & Backstage</b></h6>
-                        <p>At SMU, I am involved with the school's broadcast and entertainment society, particularly in their Events & Backstage department. I have received training in Live Sound Reinforcement, Lighting and am currently undertaking Stage Management training.</p>
                     </div>
                 </li>
             </ul>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,6 +5,12 @@ import '../App.css';
 class Portfolio extends Component {
 
     state = {
+        games : [
+            {id:1, title: "Anon", url:"https://venushmallow.github.io/Anon/", desc: "A simple narrative game where you learn basic OSINT skills and track down your missing friend. School project for Software Project Management. My role included building Levels 1-2."},
+            {id:2, title: "Housemates from Hell", url:"https://wallflower6.itch.io/housemates-from-hell", desc: "A very simple game made for GMTK Game Jam 2021 (theme: Joined Together) in under 48 hours using free assets from Kenney and OpenGameArt. For MacOS."},
+            {id:3, title: "Lame Space Collector", url:"https://wallflower6.github.io/LameSpaceCollector/", desc: "Collect stars and gems in a spacecraft but keep an eye out for asteroids! Inspired by snake, a project to try some beginner's independent work in Unity."},
+            {id:4, title: "Pong Clone", url:"https://wallflower6.github.io/PongClone/", desc: "Pong clone, drew on Noobtuts' tutorial. Added in some extras, like a trail effect, increasing speed, single-player mode, scoreboard, menu, audio."}
+        ],
         web : [
           {id:1, title: "Toothless (CSS)", url:"https://codepen.io/wallflower6/full/MLmJLj", desc: "Toothless from Dreamworks' How To Train Your Dragon, made with CSS. A simple project to celebrate the release of the third film, The Hidden World."},
           {id:2, title: "Tribute Page", url:"https://wallflower6.github.io/tribute-page/", desc: "A tribute page to Alan Turing, made as a freeCodeCamp project. The UI was inspired by the Bletchley Park bombe, but greyed out."},
@@ -13,15 +19,9 @@ class Portfolio extends Component {
           {id:5, title: "Product Landing Page", url:"https://codepen.io/wallflower6/full/omeBYx", desc: "How To Train Your Dragon-themed product landing page, a freeCodeCamp project. Frontend only, with no backend functionality."},
           {id:6, title: "Whack-A-Char", url:"https://wallflower6.github.io/whack-a-char/", desc: "Based on Wes Bos's JS30 tutorial, modified with characters from my favourite films, includes a mallet and sound effects."}
         ],
-        games : [
-            {id:1, title: "Lame Space Collector", url:"https://wallflower6.github.io/LameSpaceCollector/", desc: "Collect stars and gems in a spacecraft but keep an eye out for asteroids! Inspired by snake, a project to try some beginner's independent work in Unity."},
-            {id:2, title: "Pong Clone", url:"https://wallflower6.github.io/PongClone/", desc: "Pong clone, drew on Noobtuts' tutorial. Added in some extras, like a trail effect, increasing speed, single-player mode, scoreboard, menu, audio."},
-            {id:3, title: "Anon (Prototype)", url:"https://wallflower6.github.io/anon-prototype/", desc: "Inspired by classic adventure / mystery games, combined with cybersecurity elements."}
-        ],
         misc: [
             {id:1, title: "Blender: Doughnut", url:"https://drive.google.com/file/d/1np4tHk0nE_ebUuQckdqy7Wz482BCKMK5/view?usp=sharing", desc: "Followed Blender Guru's doughnut tutorial, and here's my final render. It was my first attempt at 3D modelling, texturing and lighting."},
             {id:2, title: "Blender: Sword Scene", url:"https://drive.google.com/file/d/1y6MLLDpA7E_KcbsmwT67G7lxyGq0-ecc/view?usp=sharing", desc: "Followed CG Fast Track's tutorial series. I got to brush up on 3D modelling, learned to use HDRIs, imported assets, was introduced to composition."},
-            {id:3, title: "Blender: Dice", url:"https://drive.google.com/file/d/1Qc9iT7sw9ULlnxcMrVvDXz0SqkzTPjJv/view?usp=sharing", desc: "Followed Rob Tuytel's tutorial, more practice with 3D modelling and lighting."}
         ]
     }
 
@@ -29,10 +29,10 @@ class Portfolio extends Component {
         return (
             <div className="container">
                 <br />
-                <h5>Web Projects</h5>
+                <h5>Game Projects</h5>
                 <br />
                 <div className="grid-container">
-                {this.state.web.map(
+                {this.state.games.map(
                     project => {
                         return (
                             <div className="grid-item" key={project.id}>
@@ -45,10 +45,10 @@ class Portfolio extends Component {
                 </div>
                 <br />
                 <br />
-                <h5>Games</h5>
+                <h5>Web Projects</h5>
                 <br />
                 <div className="grid-container">
-                {this.state.games.map(
+                {this.state.web.map(
                     project => {
                         return (
                             <div className="grid-item" key={project.id}>
